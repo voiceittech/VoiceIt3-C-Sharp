b6c7fd9acc6bbcb182c683a44ba455c50c541856
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-06-18 11:56:15 -0500

    RELEASEMINOR SENDEMAIL
    Added overloaded constructor to support using an instance of WebProxy inside VoiceIt2 client

--- VoiceIt2.cs
+++ VoiceIt2.cs
@@ -10,7 +10,7 @@ namespace VoiceIt2API
     public class VoiceIt2
     {
         const string BASE_URL = "https://api.voiceit.io";
-        const string VERSION = "2.4.0";
+        const string VERSION = "2.5.0";
         string notificationUrl = "";
         RestClient client;
 
