835712de976eca28e8127b169ab10f77acd314a8
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2026-02-13 11:49:05 -0600

    Fix API base URL typo: qpi.voiceit.io → api.voiceit.io

--- Test/release.sh
+++ Test/release.sh
@@ -159,7 +159,7 @@ then
       done
 
       json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": "true", "instructions": "nuget update</code></div><br /><p class=\"p1\"><span class=\"s1\">inside your project.</span></p><br />"}'
-      curl -X POST -H "Content-Type: application/json" -d $json "https://qpi.voiceit.io/platform/30"
+      curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/30"
     fi
 
     exit 0
